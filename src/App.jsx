8b3039fbd7be2011458822b6889df04cc30ef16c
Helen0105 <marinenkoolena21@gmail.com>
2026-08-24 21:55:42 +0300

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import user from './user.json'
+import './App.css'
+import './components/Profile'
+function App() {
+
+
+  return (
+    <>
+    
+<Profile user={user}/>
+
+
+    </>
+  )
+}
+
+export default App
